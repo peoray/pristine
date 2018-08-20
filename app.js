eb18cfc410bc603119eebf41ddfa73290d2a4f3e
peoray <peoray@yahoo.com>
2018-08-20 21:16:17 +0100

    Add views content

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('connected');
+    res.render('home');
 });
+
+app.get('/', (req, res) => {
+    res.render('')
+}
 
 app.listen(3000, () => 'server is listening on port 3000');
